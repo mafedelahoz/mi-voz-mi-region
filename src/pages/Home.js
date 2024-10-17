5ebc1e98c13001f3ca956d1752f3f9a38e197f9b
mafedelahoz <m.delah@uniandes.edu.co>
2024-10-17 13:23:34 -0500

    logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// Importa la imagen del logo
+import logo from '../assets/mivoz.png';
 
 function Home() {
   return (
     <div className="text-center">
+      {/* Agrega el logo en el encabezado */}
+      
       <h1>Bienvenido a Mi Voz, Mi Región</h1>
+      <img src={logo} alt="Logo de Mi Voz, Mi Región" className="logo" style={{ width: '600px', marginBottom: '20px' }} />
       <p>Escucha y comparte las voces de las minorías en Colombia.</p>
       <Link to="/publications" className="btn btn-primary">Explorar Publicaciones</Link>
+
     </div>
   );
 }
