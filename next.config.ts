212b585c32f4d55fa57f3d6379c1ffdf51164425
mafedelahoz <m.delah@uniandes.edu.co>
2024-10-29 22:23:15 -0500

    cambio dependencias

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true, // Habilita el modo estricto de React para detectar errores
   swcMinify: true,       // Activa la minificación de SWC para mejorar el rendimiento
-  output: 'standalone'  // Útil para optimizar el despliegue en Vercel y otras plataformas
+  output: 'standalone', // Útil para optimizar el despliegue en Vercel y otras plataformas
 };
 
 export default nextConfig;
