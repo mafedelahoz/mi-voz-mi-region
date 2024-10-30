11b36d6325ca446e33cfb3ba25b6b9b8f7bf8d11
Daniel Diaz <d.diazm@uniandes.edu.co>
2024-10-29 22:55:54 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  basePath: '/mi-voz-mi-region',
 };
 
 export default nextConfig;
