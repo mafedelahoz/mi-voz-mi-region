d8f43f231c03d9876c4340d6ca18203e4a3f3cff
mafedelahoz <m.delah@uniandes.edu.co>
2024-10-29 22:07:24 -0500

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Habilita el modo estricto de React para detectar errores
+  swcMinify: true,       // Activa la minificación de SWC para mejorar el rendimiento
+  output: 'standalone'  // Útil para optimizar el despliegue en Vercel y otras plataformas
 };
 
 export default nextConfig;
